96b8e21942aa17015559bd0881a2cf9146f29454
kimHanSeong <goalgoloo1@gmail.com>
2025-03-26 00:02:24 +0900

    [Feat] camera move

---
+++ Assets/HS/Scripts/TempData.cs
@@ -0,0 +1,5 @@
+using UnityEngine; //For the Sprite variable type
+public static class TempData
+{
+    public static GameController.CameraMonitor playerViewingCamera { get; set; } //Used in various locations to determine which camera the player is monitoring
+}
