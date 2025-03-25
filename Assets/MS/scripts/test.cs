518e1f1dff95a5c57a04f5b2ecb4343f05ddfe65
minji <kminsuk1999@gmail.com>
2025-03-25 23:00:14 +0900

    korean update

---
+++ Assets/MS/scripts/test.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    /// <summary>
+    /// 이거는 한글이 되는지 테스트
+    /// </summary>
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
