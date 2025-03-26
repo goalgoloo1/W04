43e5ce338307bc8c992c9c50140ee4aced4ab88a
kimHanSeong <goalgoloo1@gmail.com>
2025-03-26 21:32:48 +0900

    Feat timer

--- Assets/HS/Scripts/Timer.cs
+++ Assets/HS/Scripts/Timer.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    float m_seconds = 595f;
+    float m_seconds = 0f;
     int m_hours = 0;
     bool m_isTimePaused = false;
 
     void Update()
     {
-        //CheckMonsterState();
+        CheckMonsterState();
         PrintHours();
         StageClear();
         UpdateTimer();
@@ -38,22 +38,22 @@ public class Timer : MonoBehaviour
 
     //To do: 메인 씬에 넣고 아노말리 일 때 시계 멈추는지 확인
 
-    //void CheckMonsterState()
-    //{
-    //    //Debug.Log("아노말리 때 시간 멈추는 지 확인 " + m_timePaused);
+    void CheckMonsterState()
+    {
+        //Debug.Log("아노말리 때 시간 멈추는 지 확인 " + m_timePaused);
 
-    //    for (int i = 1; i < 8; i++) 
-    //    { 
-    //        //Debug.Log(MonsterManager.Instance.GetMonster(i).state);
-    //        if (i == 2) continue; // Skip monster 2 자판기 괴물 관짝행.
-    //        if (MonsterManager.Instance.GetMonster(i).state == MonsterState.Anomalous)
-    //        {
-                
-    //            m_timePaused = true;
-    //            return;
-    //        }
-    //    }
-    //    m_timePaused = false;
-    //}
+        for (int i = 1; i < 8; i++)
+        {
+            //Debug.Log(MonsterManager.Instance.GetMonster(i).state);
+            if (i == 2) continue; // Skip monster 2 자판기 괴물 관짝행.
+            if (MonsterManager.Instance.GetMonster(i).state == MonsterState.Anomalous)
+            {
+
+                m_isTimePaused = true;
+                return;
+            }
+        }
+        m_isTimePaused = false;
+    }
 }
 
