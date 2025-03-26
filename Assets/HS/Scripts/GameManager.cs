5a2ff4c4c4a37c669e0324a5a39b47fdfc5c46b7
kimHanSeong <goalgoloo1@gmail.com>
2025-03-26 22:56:37 +0900

    [Feat] GameManager 1/2

--- Assets/HS/Scripts/GameManager.cs
+++ Assets/HS/Scripts/GameManager.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject nutsJumpscareObject; // Reference to the 2D image object for Nuts jumpscare
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (MonsterManager.Instance.GetMonster(1).state == MonsterState.Critical) { NutsJumpscare(); }
+        if (MonsterManager.Instance.GetMonster(3).state == MonsterState.Critical) { MaskJumpscare(); }
+        if (MonsterManager.Instance.GetMonster(4).state == MonsterState.Critical) { AngelJumpscare(); }
+        if (MonsterManager.Instance.GetMonster(5).state == MonsterState.Critical) { SlendermanJumpscare(); }
+        if (MonsterManager.Instance.GetMonster(7).state == MonsterState.Critical) { BallerinaJumpscare(); }
+    }
+    void GameOver()
+    {
+        Debug.Log("메인화면 씬 로드");
+    }
+    void NutsJumpscare()
+    {
+        Debug.Log("Nuts한테 쥬금");
+        nutsJumpscareObject.SetActive(true); 
+        StartCoroutine(DelayedGameOver());
+    }
+    void MaskJumpscare()
+    {
+        Debug.Log("Mask한테 쥬금");
+        StartCoroutine(DelayedGameOver());
+    }
+    void AngelJumpscare()
+    {
+        Debug.Log("Angel한테 쥬금");
+        StartCoroutine(DelayedGameOver());
+    }
+    void SlendermanJumpscare()
+    {
+        Debug.Log("Slenderman한테 쥬금");
+        StartCoroutine(DelayedGameOver());
+    }
+    void BallerinaJumpscare()
+    {
+        Debug.Log("Ballerina한테 쥬금");
+        StartCoroutine(DelayedGameOver());
+    }
+    IEnumerator DelayedGameOver()
+    {
+        yield return new WaitForSeconds(2f);
+        GameOver();
     }
 }
