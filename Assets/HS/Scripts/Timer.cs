4385ba3a70440e4e88067c06634b09534aaa7080
kimHanSeong <goalgoloo1@gmail.com>
2025-03-27 09:45:43 +0900

    [Fix] timer doesn't stop case in anomaly

--- Assets/HS/Scripts/Timer.cs
+++ Assets/HS/Scripts/Timer.cs
@@ -5,13 +5,13 @@ public class Timer : MonoBehaviour
 {
     float m_seconds = 96f;
     int m_hours = 0;
-    bool m_isTimePaused = false;
+    //bool m_isTimePaused = false;
 
     [SerializeField] private TextMeshProUGUI timeDisplay;
 
     void Update()
     {
-        CheckMonsterState();
+        //CheckMonsterState();
         PrintHours();
         StageClear();
         UpdateTimer();
@@ -23,10 +23,12 @@ public class Timer : MonoBehaviour
     }
     void UpdateTimer()
     {
-        if (!m_isTimePaused)
-        {
-            m_seconds += Time.deltaTime;
-        }
+        //if (!m_isTimePaused)
+        //{
+        //    m_seconds += Time.deltaTime;
+        //}
+        m_seconds += Time.deltaTime;
+
     }
     void PrintHours()
     {
@@ -39,28 +41,27 @@ public class Timer : MonoBehaviour
     {
         if (m_hours == 6)
         {
-            m_isTimePaused = true;
             Debug.Log("Stage Clear! , Clear.Scene 로드");
         }
     }
 
     //To do: 메인 씬에 넣고 아노말리 일 때 시계 멈추는지 확인
 
-    void CheckMonsterState()
-    {
-        //Debug.Log("아노말리 때 시간 멈추는 지 확인 " + m_timePaused);
+    //void CheckMonsterState()
+    //{
+    //    //Debug.Log("아노말리 때 시간 멈추는 지 확인 " + m_timePaused);
 
-        for (int i = 1; i < 8; i++)
-        {
-            //Debug.Log(MonsterManager.Instance.GetMonster(i).state);
-            if (i == 2) continue; // Skip monster 2, 자판기 괴물 피쳐 삭제.
-            if (MonsterManager.Instance.GetMonster(i).state == MonsterState.Anomalous)
-            {
-                m_isTimePaused = true;
-                return;
-            }
-        }
-        m_isTimePaused = false;
-    }
+    //    for (int i = 1; i < 8; i++)
+    //    {
+    //        //Debug.Log(MonsterManager.Instance.GetMonster(i).state);
+    //        if (i == 2) continue; // Skip monster 2, 자판기 괴물 피쳐 삭제.
+    //        if (MonsterManager.Instance.GetMonster(i).state == MonsterState.Anomalous)
+    //        {
+    //            m_isTimePaused = true;
+    //            return;
+    //        }
+    //    }
+    //    m_isTimePaused = false;
+    //}
 }
 
