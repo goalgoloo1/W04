8af86e3fd331715a3e1e4d11ce78261378e6ab4d
kimHanSeong <goalgoloo1@gmail.com>
2025-03-28 15:16:56 +0900

    [Feat] WarningScene

---
+++ Assets/HS/Scripts/FadingWarning.cs
@@ -0,0 +1,49 @@
+using DG.Tweening;
+using UnityEngine;
+using System.Collections;
+
+public class FadingWarning : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    private Tween m_fadeTween;
+    void Start()
+    {
+        StartCoroutine(TestFade());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FadeIn(float duration)
+    {
+        Fade(1f, duration, () => 
+        {
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        });
+    }
+    public void FadeOut(float duration)
+    {
+        Fade(0f, duration, () =>
+        {
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        });
+    }
+    void Fade(float endValue, float duration, TweenCallback onEnd)
+    {
+        if (m_fadeTween != null)
+        {
+            m_fadeTween.Kill();
+        }
+        m_fadeTween = canvasGroup.DOFade(endValue, duration).OnComplete(onEnd);
+    }
+    IEnumerator TestFade()
+    {
+        FadeIn(1f);
+        yield return new WaitForSeconds(3f);
+        FadeOut(0.3f);
+    }
+}
